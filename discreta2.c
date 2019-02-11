2c03867c356d15839824c94f5705be90ba874af4
Lucas Buarque <47061990+nLucasx@users.noreply.github.com>
2019-02-11 12:41:37 -0300

    MDC como combinacao linear

---
+++ discreta2.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+void mdc(int a, int b);
+
+int main()
+{
+	int a, b;
+
+	scanf("%d%d", &a, &b);
+	mdc(a,b);
+	return 0;
+}
+void mdc(int a, int b)
+{
+	int quocientes[100000], i = 0, aux, j, r, resposta[100000], k =0, finala, finalb;
+	
+	if (b > a)
+	{
+		aux = b;
+		b = a;
+		a = aux;
+	}
+	finala = a;
+	finalb = b;
+	while (b != 0)
+	{
+		quocientes[i] = a / b;
+		i++;
+		r = a % b;
+		a = b;
+		b = r;
+		
+	}
+	resposta[0] = 1;
+	for (j = i-2; j >= 0; j--)
+	{
+		if (k == 0)
+		{
+			resposta[k+1] = quocientes[j] * resposta[k];
+			k++;
+		}
+		else
+		{
+			resposta[k+1] = (quocientes[j] * resposta[k]) + (resposta[k-1]);
+			k++;
+		}
+	}
+	if (k % 2 == 0)
+	{
+		system("clear");
+		printf("MDC = COMBINACAO LINEAR ENTRE %d %d:\n", finala, finalb);
+		printf("%d = (-%d) * %d + (%d) * %d", a, resposta[k-1], finala, resposta[k], finalb);
+	}
+	else
+	{
+		system("clear");
+		printf("MDC = COMBINACAO LINEAR ENTRE %d %d:\n", finala, finalb);
+		printf("%d = (%d) * %d + (-%d) * %d", a, resposta[k-1], finala, resposta[k], finalb);	
+	}
+	printf("\n");
+}
